fb7495d02f6f787fdf6abedef35353f8a9f89ee1
Nahueladmin <nahuroldan1012@gmail.com>
2024-10-17 16:08:42 -0300

    Add files via upload

--- scripts_resumen.js
+++ scripts_resumen.js
@@ -1,96 +1,61 @@
 window.onload = function ValoresCondimento1() {
-    
     const paqueteJSON = localStorage.getItem("Corte1");
-    
-        // Convertir la cadena JSON en un objeto JavaScript
-        const paquete = JSON.parse(paqueteJSON);
+    const paquete = JSON.parse(paqueteJSON);
 
-        // Ahora puedes acceder a las propiedades del objeto
-        console.log("Corte1: " + paquete.corte);
-        console.log("Punto de Cocción1: " + paquete.puntoCoccion);
-        console.log("Sal1: " + paquete.sal);
-        console.log("Chimi1: " + paquete.chimi);
-        console.log("Smoked1: " + paquete.smoke);
-
+    if (paquete) {
         document.getElementById("valueSal1").innerHTML = paquete.sal;
         document.getElementById("valueChimi1").innerHTML = paquete.chimi;
         document.getElementById("valueSmoke1").innerHTML = paquete.smoke;
+        document.getElementById("puntoCoccion1").innerHTML = paquete.puntoCoccion; // Mostrar punto de cocción
 
-    if (paquete.corte === "pollo") 
-    {
-    document.getElementById("displayedImageGroup1").src = 'images/POLLOBUENO.png';
-    }
-    else if (paquete.corte === "carne") {
-    document.getElementById("displayedImageGroup1").src = 'images/CARNEBUENA.png';
-    }
-    else if (paquete.corte === "cerdo") {
-    document.getElementById("displayedImageGroup1").src = 'images/CERDOBUENO.png';
-    }
-    else if (paquete.corte === "cruz") {
-        document.getElementById("displayedImageGroup1").src = 'images/CRUZBUENA.png';
+        if (paquete.corte === "pollo") {
+            document.getElementById("displayedImageGroup1").src = 'images/POLLOBUENO.png';
+        } else if (paquete.corte === "carne") {
+            document.getElementById("displayedImageGroup1").src = 'images/CARNEBUENA.png';
+        } else if (paquete.corte === "cerdo") {
+            document.getElementById("displayedImageGroup1").src = 'images/CERDOBUENO.png';
+        } else if (paquete.corte === "cruz") {
+            document.getElementById("displayedImageGroup1").src = 'images/CRUZBUENA.png';
         }
-    
-
+    }
 
     const dospaqueteJSON = localStorage.getItem("Corte2");
-    
+    const paquetedos = JSON.parse(dospaqueteJSON);
 
-        // Convertir la cadena JSON en un objeto JavaScript
-        const paquetedos = JSON.parse(dospaqueteJSON);
-
-        // Ahora puedes acceder a las propiedades del objeto
-        console.log("Corte2: " + paquetedos.corte);
-        console.log("Punto de Cocción2: " + paquetedos.puntoCoccion);
-        console.log("Sal2: " + paquetedos.sal);
-        console.log("Chimi2: " + paquetedos.chimi);
-        console.log("Smoked2: " + paquetedos.smoke);
-
+    if (paquetedos) {
         document.getElementById("valueSal2").innerHTML = paquetedos.sal;
         document.getElementById("valueChimi2").innerHTML = paquetedos.chimi;
         document.getElementById("valueSmoke2").innerHTML = paquetedos.smoke;
+        document.getElementById("puntoCoccion2").innerHTML = paquetedos.puntoCoccion; // Mostrar punto de cocción
 
-        if (paquetedos.corte === "pollo") 
-    {
-    document.getElementById("displayedImageGroup2").src = 'images/POLLOBUENO.png';
-    }
-    else if (paquetedos.corte === "carne") {
-    document.getElementById("displayedImageGroup2").src = 'images/CARNEBUENA.png';
-    }
-    else if (paquetedos.corte === "cerdo") {
-    document.getElementById("displayedImageGroup2").src = 'images/CERDOBUENO.png';
-    }
-    else if (paquetedos.corte === "cruz") {
-        document.getElementById("displayedImageGroup2").src = 'images/CRUZBUENA.png';
+        if (paquetedos.corte === "pollo") {
+            document.getElementById("displayedImageGroup2").src = 'images/POLLOBUENO.png';
+        } else if (paquetedos.corte === "carne") {
+            document.getElementById("displayedImageGroup2").src = 'images/CARNEBUENA.png';
+        } else if (paquetedos.corte === "cerdo") {
+            document.getElementById("displayedImageGroup2").src = 'images/CERDOBUENO.png';
+        } else if (paquetedos.corte === "cruz") {
+            document.getElementById("displayedImageGroup2").src = 'images/CRUZBUENA.png';
         }
+    }
 
     const trespaqueteJSON = localStorage.getItem("Corte3");
-    
+    const paquetetres = JSON.parse(trespaqueteJSON);
 
-        // Convertir la cadena JSON en un objeto JavaScript
-        const paquetetres = JSON.parse(trespaqueteJSON);
-
-        // Ahora puedes acceder a las propiedades del objeto
-        console.log("Corte3: " + paquetetres.corte);
-        console.log("Punto de Cocción3: " + paquetetres.puntoCoccion);
-        console.log("Sal3: " + paquetetres.sal);
-        console.log("Chimi3: " + paquetetres.chimi);
-        console.log("Smoked3: " + paquetetres.smoke);
-
+    if (paquetetres) {
         document.getElementById("valueSal3").innerHTML = paquetetres.sal;
         document.getElementById("valueChimi3").innerHTML = paquetetres.chimi;
         document.getElementById("valueSmoke3").innerHTML = paquetetres.smoke;
+        document.getElementById("puntoCoccion3").innerHTML = paquetetres.puntoCoccion; // Mostrar punto de cocción
 
-    if (paquetetres.corte === "pollo") 
-    {
-    document.getElementById("displayedImageGroup3").src = 'images/POLLOBUENO.png';
-    }
-    else if (paquetetres.corte === "carne") {
-    document.getElementById("displayedImageGroup3").src = 'images/CARNEBUENA.png';
-    }
-    else if (paquetetres.corte === "cerdo") {
-    document.getElementById("displayedImageGroup3").src = 'images/CERDOBUENO.png';
-    }
-    else if (paquetetres.corte === "cruz") {
-        document.getElementById("displayedImageGroup3").src = 'images/CRUZBUENA.png';
+        if (paquetetres.corte === "pollo") {
+            document.getElementById("displayedImageGroup3").src = 'images/POLLOBUENO.png';
+        } else if (paquetetres.corte === "carne") {
+            document.getElementById("displayedImageGroup3").src = 'images/CARNEBUENA.png';
+        } else if (paquetetres.corte === "cerdo") {
+            document.getElementById("displayedImageGroup3").src = 'images/CERDOBUENO.png';
+        } else if (paquetetres.corte === "cruz") {
+            document.getElementById("displayedImageGroup3").src = 'images/CRUZBUENA.png';
         }
+    }
 }
